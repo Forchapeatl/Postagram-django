ffbcc324d206a4eab8f9e5d9204e41fd398bd88f
FORCHA <forchapearl1@gmail.com>
2023-07-20 09:41:29 +0100

    Update settings.py

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if ENV == "PROD" else True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
